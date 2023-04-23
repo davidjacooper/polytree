d3678c3f9eb1fcf83a802b0601926dea204ddc30
David Cooper <david.cooper@curtin.edu.au>
2023-04-23 17:24:15 +0800

    Added copyright message.

--- app/src/main/java/edu/curtin/polyfind/PolyFind.java
+++ app/src/main/java/edu/curtin/polyfind/PolyFind.java
@@ -12,7 +12,8 @@ import java.util.concurrent.Callable;
 @CommandLine.Command(name = "polyfind",
                      mixinStandardHelpOptions = true,
                      version = "0.1",
-                     description = "Experimental tool to display the structure of a student's Java application.")
+                     description = "Experimental tool to display the structure of a student's Java application.",
+                     footer = "Copyright (c) 2023 by David J A Cooper.")
 public class PolyFind implements Callable<Integer>
 {
     public static void main(String[] args) 
@@ -20,11 +21,11 @@ public class PolyFind implements Callable<Integer>
         System.exit(new CommandLine(new PolyFind()).execute(args));
     }
     
-    @CommandLine.Parameters(index = "0", description = "Root of the source code directory tree.")
+    @CommandLine.Parameters(index = "0", description = "Root of the source code directory tree to parse and display.")
     private File directory;
     
     @CommandLine.Option(names = {"-a", "--ascii"}, 
-                        description = "Use standard ASCII symbols only (in case non-ASCII Unicode doesn't display properly).")
+                        description = "Use standard ASCII symbols only (if non-ASCII box-drawing symbols don't display properly).")
     private boolean ascii;
     
     @Override
